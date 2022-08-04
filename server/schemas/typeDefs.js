f900f2bc5dc47f9d9ef98d17944285515eb8fc71
Jacob  Bauer <jacobbauer@Jacobs-Air.hitronhub.home>
2022-08-04 14:45:39 -0500

    created user queries

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,11 +9,30 @@ const typeDefs = gql`
 		createdAt: String
 		username: String
 		reactionCount: Int
+		reactions: [Reaction]
+	}
+	type Reaction {
+		_id: ID
+		reactionBody: String
+		createdAt: String
+		username: String
+	}
+	type User {
+		_id: ID
+		username: String
+		email: String
+		friendCount: Int
+		thoughts: [Thought]
+		friends: [User]
 	}
 	type Query {
-		thoughts: [Thought]
+		users: [User]
+		user(username: String!): User
+		thoughts(username: String): [Thought]
+		thought(_id: ID!): Thought
 	}
 `;
 
+// ! means data must exists before it is carried out
 // export the typeDefs
 module.exports = typeDefs;
